5d4500cff406b51008a7b935f41922c3234ae97c
SouhirMahmoudi <souhir.mahmoudi2021@campus-eni.fr>
2022-07-06 12:04:19 +0200

    feat: creation de composent footer.jsx

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './footer.scss'
+
+export default function Footer() {
+
+    return (
+        <div className="footer">
+            <img src="../LOGO.svg" alt="Kasa LOGO" />
+            <p>© 2020 Kasa. All rights reserved</p>
+        </div>
+    )
+
+}
