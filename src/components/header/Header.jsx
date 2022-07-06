c498ea30174d429f3cb55a54677e797b816cf0eb
SouhirMahmoudi <souhir.mahmoudi2021@campus-eni.fr>
2022-07-06 11:54:03 +0200

    feat: modification classe header / home page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,16 @@
-import logo from "./logo.svg"
-import { NavLink } from 'react-router-dom'
- 
-function Header() {
+import { Link } from 'react-router-dom'
+import './Header.css'
+
+export default function Header() {
+
     return (
-        <div className="navbar">
-        <nav>
-            <NavLink to="/"><img className="logo" src={logo}/></NavLink>
-        <div className="navLinks">
-            <NavLink to="/home">Accueil</NavLink>
-            <NavLink to="/about">A propos</NavLink>
-            </div> 
-        </nav>
+        <div className="header">
+            <img src="../kasa.svg" alt="Kasa LOGO" />
+            <div className="navbar">
+                <Link to="/">Accueil</Link>
+                <Link to="/about">A propos</Link>
+            </div>
         </div>
     )
+
 }
-
-export default Header
-
--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
+import React from 'react'
+import { render } from 'react-dom'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import ErrorFound from './components/ErrorFound/ErrorFound'
+import Home from './pages/Home/Home'
+import Header from './components/Header/Header'
+import './index.css'
+import LocationPage from './pages/LocationPage/LocationPage'
+import About from './pages/About/About'
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+  <BrowserRouter>
+    <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/locations/:id' component={LocationPage} />
+        <Route exact path='/about' component={About} />
+        <Route component={ErrorFound} />
+      </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
-);
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
