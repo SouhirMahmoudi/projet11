9c259dd0641abc42426476e2a84b3610457e2930
SouhirMahmoudi <souhir.mahmoudi2021@campus-eni.fr>
2022-07-12 18:15:11 +0200

    style: ajustement css page home

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-import CardHome from 'components/cardHome/CardHome'
+import CardHome from "components/cardHome/CardHome";
 import { getAll } from "services/dataManager";
+import "components/gallery/gallery.scss";
 
-export default function Gallery () {
-   const homeData = getAll();
-      return (
-        <div className="gallery">
-          {homeData.map((home) => (
-            <CardHome
-              key={home.id}
-              id={home.id}
-              img={home.cover}
-              title={home.title}
-            />
-          ))}
-        </div>
-      );
-    }
-  
+export default function Gallery() {
+  const homeData = getAll();
+  return (
+    <div className="gallery">
+      <div className="articles">
+        {homeData.map((home) => (
+          <CardHome
+            key={home.id}
+            id={home.id}
+            img={home.cover}
+            title={home.title}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
